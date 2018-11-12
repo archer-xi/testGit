7f77d2817fbc2a9b883c1454feb9e9716f3ff5c8
archer.xi <xzq19891127@163.com>
2018-11-12 22:21:28 +0800

    XXX

---
+++ testSVN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSVN
+//
+//  Created by Archer on 2018/11/12.
+//  Copyright © 2018年 Archer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
